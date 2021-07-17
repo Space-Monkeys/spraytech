76b36072585fb1629702763ea5be7b805f87239f
Kelviny Henrique <kelvinyhenrique17@gmail.com>
2021-07-17 11:06:05 -0300

    ¯\_( ͡° ͜ʖ ͡°)_/¯

--- App.js
+++ App.js
@@ -3,8 +3,7 @@ import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './src/screens/Home';
-import Details from './src/screens/Details'
-
+import Details from './src/screens/Details';
 
 const Stack = createStackNavigator();
 
@@ -12,10 +11,20 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home}  options={{
-          headerShown:false
-        }}/>
-        <Stack.Screen name="Details" component={Details} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
