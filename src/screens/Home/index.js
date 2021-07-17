98849e4bd6359f968896fe6f988fd3061fc03884
Kelviny Henrique <kelvinyhenrique17@gmail.com>
2021-07-17 09:24:14 -0300

    Add new components

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import UserWidget from '../../components/UserWidget';
 import Weather from '../../components/Weather';
 import styles from './styles';
+import Search from '../../components/Search';
+import BreadCrumb from '../../components/BreadCrumb';
+import Vehicle from '../../components/Vehicle';
 
 export default function HomeScreen({ navigation }) {
   return (
@@ -19,6 +22,18 @@ export default function HomeScreen({ navigation }) {
     >
       <UserWidget />
       <Weather />
+      <View style={styles.SearchContainer}>
+        <Text style={{ marginBottom: 10 }}>Pesquisar Veículos:</Text>
+        <Search />
+      </View>
+
+      <BreadCrumb />
+      <ScrollView style={styles.ScrollView} horizontal={false}>
+        <Vehicle />
+        <Vehicle />
+        <Vehicle />
+        <Vehicle />
+      </ScrollView>
     </View>
   );
 }
