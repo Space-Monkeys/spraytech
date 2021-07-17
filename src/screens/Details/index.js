673a95bc81f185f729bac3f1818bc18fa20562c2
Kelviny Henrique <kelvinyhenrique17@gmail.com>
2021-07-17 20:10:37 -0300

    Implement components in screen

--- src/screens/Details/index.js
+++ src/screens/Details/index.js
@@ -1,10 +1,101 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import styles from './styles';
+import { AlertTriangle } from 'react-native-feather';
+import Specification from '../../components/Specification';
+import Inspection from '../../components/Inspection';
 
 export default function DetailsScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+      }}
+    >
+      <View style={styles.VehicleDetail}>
+        <Image
+          style={styles.userImage}
+          source={require('../../../assets/pulverizadores/boxer2000hFull.png')}
+        />
+      </View>
+      <TouchableOpacity style={styles.BtnInspection}>
+        <View>
+          <Text style={styles.TextButton}>INICIAR</Text>
+          <Text style={styles.TextButton}>INSPEÇÃO</Text>
+        </View>
+        <AlertTriangle width={24} stroke="#FFF" height={25} />
+      </TouchableOpacity>
+      <ScrollView horizontal={false} vertical={true}>
+        <View>
+          <View style={styles.ContainerDetails}>
+            <View style={styles.RightContent}>
+              <Text>Odômetro</Text>
+              <Text style={{ fontWeight: 'bold' }}>200h</Text>
+            </View>
+            <View style={styles.CenterContent}>
+              <Text>Status</Text>
+              <View style={styles.TagStatus}>
+                <Text
+                  style={{ fontWeight: 'bold', fontSize: 10, color: '#ffb703' }}
+                >
+                  Ocioso
+                </Text>
+              </View>
+            </View>
+            <View style={styles.LeftContent}>
+              <Text>Última Inspeção</Text>
+              <Text style={{ fontWeight: 'bold' }}>3 dias</Text>
+            </View>
+          </View>
+        </View>
+        <Text style={styles.SpecsText}>Especificações</Text>
+        <View style={styles.ScrollView}>
+          <ScrollView
+            style={styles.ScrollView}
+            horizontal={true}
+            vertical={false}
+          >
+            <Specification
+              description={'Tanque de Pulverização'}
+              icon={require('../../../assets/icons/fertilizer_tank.png')}
+              value={'2.000L'}
+            />
+            <Specification
+              description={'Potência do Motor '}
+              icon={require('../../../assets/icons/engine_motor.png')}
+              value={'190CV'}
+            />
+            <Specification
+              description={'Teste'}
+              icon={require('../../../assets/icons/engine_motor.png')}
+              value={'190CV'}
+            />
+            <Specification
+              description={'Teste'}
+              icon={require('../../../assets/icons/engine_motor.png')}
+              value={'190CV'}
+            />
+          </ScrollView>
+        </View>
+        <View>
+          <Text>Hístorico</Text>
+          <View>
+            <Inspection />
+            <Inspection />
+            <Inspection />
+            <Inspection />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
--- src/screens/Details/styles.js
+++ src/screens/Details/styles.js
@@ -1,5 +1,87 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  Teste: {},
+  container: {
+    flex: 1,
+  },
+  VehicleDetail: {
+    backgroundColor: '#1D2222',
+    height: 269,
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  userImage: {
+    marginBottom: -70,
+  },
+  BtnInspection: {
+    backgroundColor: '#008847',
+    width: 126,
+    height: 47,
+    borderRadius: 8,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15,
+    marginTop: 70,
+  },
+  TextButton: {
+    fontSize: 10,
+    marginBottom: 1,
+    color: '#FFF',
+    fontWeight: '700',
+  },
+  ContainerDetails: {
+    display: 'flex',
+    width: '100%',
+    marginTop: 20,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  TagStatus: {
+    width: 95,
+    marginTop: 3,
+    height: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    backgroundColor: '#ffb7034f',
+  },
+  LeftContent: {
+    width: '33%',
+    alignItems: 'center',
+  },
+  RightContent: {
+    width: '33%',
+    alignItems: 'center',
+  },
+  CenterContent: {
+    borderRightWidth: 0.5,
+    borderLeftWidth: 0.5,
+    borderColor: '#DBDBDB',
+    width: '33%',
+    alignItems: 'center',
+  },
+  ScrollView: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  ScrollViewBody: {
+    width: '100%',
+    height: 140,
+  },
+  SpecsText: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    fontSize: 14,
+    paddingRight: 20,
+    paddingLeft: 20,
+    marginTop: 20,
+    fontWeight: '700',
+  },
 });
