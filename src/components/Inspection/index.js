26a062a5eb3c9981727de8b1c1774544e1a015e7
Kelviny Henrique <kelvinyhenrique17@gmail.com>
2021-07-17 20:10:05 -0300

    Create inspection component

---
+++ src/components/Inspection/index.js
@@ -0,0 +1,11 @@
+import styles from './styles';
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Inspection() {
+  return (
+    <View style={styles.BoxInspection}>
+      <Text>Helooooooo</Text>
+    </View>
+  );
+}
---
+++ src/components/Inspection/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  BoxInspection: {
+    backgroundColor: '#FFF',
+    height: 231,
+    width: '100%',
+  },
+});
