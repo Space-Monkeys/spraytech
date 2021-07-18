e15d051b3ba87042926011c0c9a088fbd705034f
Kelviny Henrique <kelvinyhenrique17@gmail.com>
2021-07-18 02:32:55 -0300

    Create start inspection screen

--- src/screens/StartInspection/index.js
+++ src/screens/StartInspection/index.js
@@ -8,10 +8,7 @@ import {
   ImageBackground,
   Image,
 } from 'react-native';
-import {
-  AnimatedGaugeProgress,
-  GaugeProgress,
-} from 'react-native-simple-gauge';
+import { Clock, Settings, Users, ChevronRight, X } from 'react-native-feather';
 import styles from './styles';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -27,9 +24,6 @@ export default function StartInspection({ navigation }) {
       <View
         style={{
           width: '100%',
-          borderBottomLeftRadius: 18,
-          borderBottomRightRadius: 18,
-          backgroundColor: '#7159c1',
         }}
       >
         <ImageBackground
@@ -45,8 +39,232 @@ export default function StartInspection({ navigation }) {
             <View style={styles.Chart}>
               <Image source={require('../../../assets/chart.png')} />
             </View>
+            <View
+              style={{
+                width: '100%',
+                justifyContent: 'space-around',
+                flexDirection: 'row',
+              }}
+            >
+              <View style={styles.BoxInfo}>
+                <View
+                  style={{ backgroundColor: '#7ACEFA31', ...styles.BoxIcon }}
+                >
+                  <Users
+                    stroke="#7ACEFA"
+                    strokeWidth={3}
+                    width={22}
+                    height={22}
+                  />
+                </View>
+                <Text style={styles.Value}>121x</Text>
+                <Text style={styles.Title}>Aplicação</Text>
+              </View>
+              <View style={styles.BoxInfo}>
+                <View style={{ backgroundColor: '#FBE4E0', ...styles.BoxIcon }}>
+                  <Settings
+                    stroke="#E64A30"
+                    strokeWidth={3}
+                    width={22}
+                    height={22}
+                  />
+                </View>
+                <Text style={styles.Value}>25%</Text>
+                <Text style={styles.Title}>Taxa de Falhas</Text>
+              </View>
+              <View style={styles.BoxInfo}>
+                <View style={{ backgroundColor: '#FEF6EC', ...styles.BoxIcon }}>
+                  <Clock
+                    stroke="#F7C480"
+                    strokeWidth={3}
+                    width={22}
+                    height={22}
+                  />
+                </View>
+                <Text style={styles.Value}>50 min</Text>
+                <Text style={styles.Title}>Tempo médio de inspeção</Text>
+              </View>
+            </View>
           </View>
         </ImageBackground>
+        <View>
+          <Text style={styles.StartInspectionTitle}>
+            Selecione para Inspecionar
+          </Text>
+          <ScrollView
+            style={styles.ScrollView}
+            vertical={true}
+            horizontal={false}
+          >
+            <View style={styles.RowBox}>
+              <View style={styles.RowContainer}>
+                <View style={styles.LeftContent}>
+                  <View
+                    style={{
+                      backgroundColor: '#E64A3031',
+                      width: 40,
+                      height: 40,
+                      borderRadius: 8,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <X
+                      width={22}
+                      height={22}
+                      strokeWidth={3}
+                      stroke="#E64A30"
+                    />
+                  </View>
+                  <View style={styles.DescriptionLabel}>
+                    <Text style={styles.InspectionComponent}>Vazamentos</Text>
+                    <Text style={styles.InspectionStatus}>Não Iniciado</Text>
+                  </View>
+                </View>
+                <TouchableOpacity>
+                  <ChevronRight
+                    width={22}
+                    height={22}
+                    strokeWidth={3}
+                    stroke="#E64A30"
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.RowContainer}>
+                <View style={styles.LeftContent}>
+                  <View
+                    style={{
+                      backgroundColor: '#E64A3031',
+                      width: 40,
+                      height: 40,
+                      borderRadius: 8,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <X
+                      width={22}
+                      height={22}
+                      strokeWidth={3}
+                      stroke="#E64A30"
+                    />
+                  </View>
+                  <View style={styles.DescriptionLabel}>
+                    <Text style={styles.InspectionComponent}>Vazamentos</Text>
+                    <Text style={styles.InspectionStatus}>Não Iniciado</Text>
+                  </View>
+                </View>
+                <TouchableOpacity>
+                  <ChevronRight
+                    width={22}
+                    height={22}
+                    strokeWidth={3}
+                    stroke="#E64A30"
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.RowContainer}>
+                <View style={styles.LeftContent}>
+                  <View
+                    style={{
+                      backgroundColor: '#E64A3031',
+                      width: 40,
+                      height: 40,
+                      borderRadius: 8,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <X
+                      width={22}
+                      height={22}
+                      strokeWidth={3}
+                      stroke="#E64A30"
+                    />
+                  </View>
+                  <View style={styles.DescriptionLabel}>
+                    <Text style={styles.InspectionComponent}>Vazamentos</Text>
+                    <Text style={styles.InspectionStatus}>Não Iniciado</Text>
+                  </View>
+                </View>
+                <TouchableOpacity>
+                  <ChevronRight
+                    width={22}
+                    height={22}
+                    strokeWidth={3}
+                    stroke="#E64A30"
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.RowContainer}>
+                <View style={styles.LeftContent}>
+                  <View
+                    style={{
+                      backgroundColor: '#E64A3031',
+                      width: 40,
+                      height: 40,
+                      borderRadius: 8,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <X
+                      width={22}
+                      height={22}
+                      strokeWidth={3}
+                      stroke="#E64A30"
+                    />
+                  </View>
+                  <View style={styles.DescriptionLabel}>
+                    <Text style={styles.InspectionComponent}>Vazamentos</Text>
+                    <Text style={styles.InspectionStatus}>Não Iniciado</Text>
+                  </View>
+                </View>
+                <TouchableOpacity>
+                  <ChevronRight
+                    width={22}
+                    height={22}
+                    strokeWidth={3}
+                    stroke="#E64A30"
+                  />
+                </TouchableOpacity>
+              </View>
+              <View style={styles.RowContainer}>
+                <View style={styles.LeftContent}>
+                  <View
+                    style={{
+                      backgroundColor: '#E64A3031',
+                      width: 40,
+                      height: 40,
+                      borderRadius: 8,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <X
+                      width={22}
+                      height={22}
+                      strokeWidth={3}
+                      stroke="#E64A30"
+                    />
+                  </View>
+                  <View style={styles.DescriptionLabel}>
+                    <Text style={styles.InspectionComponent}>Vazamentos</Text>
+                    <Text style={styles.InspectionStatus}>Não Iniciado</Text>
+                  </View>
+                </View>
+                <TouchableOpacity>
+                  <ChevronRight
+                    width={22}
+                    height={22}
+                    strokeWidth={3}
+                    stroke="#E64A30"
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </ScrollView>
+        </View>
       </View>
     </View>
   );
--- src/screens/StartInspection/styles.js
+++ src/screens/StartInspection/styles.js
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   BoxContainerHome: {
     flex: 1,
-    backgroundColor: '#fff',
   },
   SearchContainer: {
     width: '100%',
@@ -27,5 +26,80 @@ export default StyleSheet.create({
     width: '70%',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  BoxInfo: {
+    width: '25%',
+    height: 115,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  BoxIcon: {
+    width: 43,
+    height: 55,
+    borderBottomRightRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 10,
+    borderBottomLeftRadius: 8,
+  },
+  Value: {
+    marginTop: 10,
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+  Title: {
+    fontSize: 10,
+    color: '#5F5F5F',
+    textAlign: 'center',
+  },
+  RowBox: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 20,
+    backgroundColor: '#FFF',
+    marginRight: 10,
+    marginLeft: 10,
+    padding: 10,
+    borderRadius: 8,
+  },
+  ScrollView: {
+    width: '100%',
+    height: 279,
+    marginTop: 10,
+  },
+  RowContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 15,
+    marginTop: 10,
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#E8E8E8',
+  },
+  LeftContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  DescriptionLabel: {
+    marginLeft: 8,
+  },
+  InspectionComponent: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  InspectionStatus: {
+    marginTop: 2,
+    fontSize: 12,
+    color: '#ADB1B2',
+  },
+  StartInspectionTitle: {
+    paddingRight: 10,
+    paddingLeft: 10,
+    fontSize: 14,
+    marginTop: 20,
+    fontWeight: 'bold',
   },
 });
